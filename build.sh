c14a3784b9b043ff31988c38ee55f0067656025c
Jon Topper <jon@scalefactory.com>
2020-11-01 17:58:07 +0000

    Add db credentals to default config

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ install --mode=0755 ./bin/MessageSpec /usr/bin/
 install --mode=0755 ./bin/ua /usr/bin/
 install --mode=0755 ./bin/libua /usr/libexec/
 install --mode=0644 ./bin/ua.edf /etc/ua2/
-install --mode=0644 ./bin/uadata.edf /var/lib/ua2/
+install --mode=0644 ./dist/uadata.edf /var/lib/ua2/
 install --mode=0755 ./dist/init /etc/init.d/ua2-server
 install --mode=0644 ./dist/logrotate /etc/logrotate.d/ua2-server
 
